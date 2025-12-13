6a2ec88ec044744ceaaf0f738928613b15ba6428
paufranch <pfranch08@gmail.com>
2025-12-13 13:45:54 +0100

    Update speech-to-text with list storage

--- app/lib/widgets/speech.dart
+++ app/lib/widgets/speech.dart
@@ -2,40 +2,47 @@ import 'package:flutter/material.dart';
 import 'package:speech_to_text_ultra/speech_to_text_ultra.dart';
 
 class STTUWidget extends StatefulWidget {
-  const STTUWidget({super.key});
+    final Function(bool)? onListeningChanged;
+    final Function(List<String>)? onWordsUpdated;
 
-  @override
-  State<STTUWidget> createState() => STTUState();
+    const STTUWidget({super.key, this.onListeningChanged, this.onWordsUpdated});
+
+    @override
+    State<STTUWidget> createState() => STTUState();
 }
 
 class STTUState extends State<STTUWidget> {
-  bool mIsListening = false;
-  String mResponse = '';
-  String mLiveResponse = '';
+    bool mIsListening = false;
+    String mResponse = '';
+    String mLiveResponse = '';
+    List<String> mWords = [];
 
-  @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            mIsListening ? '$mResponse $mLiveResponse' : mResponse,
-            textAlign: TextAlign.center,
-          ),
-          const SizedBox(height: 20),
-          SpeechToTextUltra(
-            ultraCallback:
-                (String liveText, String finalText, bool isListening) {
-              setState(() {
-                mLiveResponse = liveText;
-                mResponse = finalText;
-                mIsListening = isListening;
-              });
-            },
-          ),
-        ],
-      ),
-    );
-  }
+    @override
+    Widget build(BuildContext context) {
+        return SingleChildScrollView(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+            Text(
+                mIsListening ? '$mResponse $mLiveResponse' : mResponse,
+                textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20),
+            SpeechToTextUltra(
+                ultraCallback:
+                    (String liveText, String finalText, bool isListening) {
+                    setState(() {
+                        mLiveResponse = liveText;
+                        mResponse = finalText;
+                        mIsListening = isListening;
+                        widget.onListeningChanged?.call(mIsListening);
+                        mWords = mLiveResponse.split(' ');
+                        widget.onWordsUpdated?.call(mWords);
+                    });
+                },
+            ),
+            ],
+        ),
+        );
+    }
 }
