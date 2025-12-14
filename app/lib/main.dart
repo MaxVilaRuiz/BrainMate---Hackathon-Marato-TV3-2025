b72bcf2c2359bc571a4062a843bbe1a499aaa1fd
Max Vilà Ruiz <max16506@gmail.com>
2025-12-14 08:56:18 +0100

    Update app startup config to rebuild the shared preferences data

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -2,9 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'widgets/main_scaffold.dart';
 
+const String firstRunKey = 'has_initialized_app';
+const String dailyEntriesKey = 'daily_entries';
+const String completedDaysKey = 'completed_days';
+const String dateKey = 'date';
+
 Future<void> main() async {
-  // TODO: Delete this part to not initialize with predefined questionnaires (only for testing)
   WidgetsFlutterBinding.ensureInitialized();
+
+  // TODO: Delete after testing
+  // Predefined data
+  final predefinedCompletedDays = [
+    "2025-12-13",
+    "2025-12-12",
+    "2025-12-11",
+    "2025-12-10",
+    "2025-12-09",
+    "2025-12-08",
+  ];
 
   final predefinedQuestionnaires = [
     '{"date":"2025-12-13","diagnosis":"Atenció"}',
@@ -17,9 +32,20 @@ Future<void> main() async {
 
   final prefs = await SharedPreferences.getInstance();
 
-  // Only store if they do not exist
-  if (!prefs.containsKey('daily_entries')) {
-    await prefs.setStringList('daily_entries', predefinedQuestionnaires);
+  final isFirstRun = !prefs.containsKey(firstRunKey);
+
+  if (isFirstRun) {
+    // Clean the shared Preferences
+    await prefs.remove(dailyEntriesKey);
+    await prefs.remove(completedDaysKey);
+    await prefs.remove(dateKey);
+
+    // Initial storage
+    await prefs.setStringList(dailyEntriesKey, predefinedQuestionnaires);
+    await prefs.setStringList(completedDaysKey, predefinedCompletedDays);
+
+    // Mark initialize date
+    await prefs.setBool(firstRunKey, true);
   }
 
   runApp(const MyApp());
